8d843c383429598b1b709a03e8bd1922846d01f3
Young Koh <youngheon6@gmail.com>
2023-04-04 12:26:36 -0600

    test: write additional tests

--- app/models/application.rb
+++ app/models/application.rb
@@ -14,18 +14,21 @@ class Application < ApplicationRecord
 
   def update_status(application_pets)
     application = application_pets.first.application
-    
     if application_pets.any? {|ap| ap.approved == false}
       application.update(status: "Rejected!")
     elsif application_pets.any? {|ap| ap.approved == nil}
       application.update(status: "In Progress")
     else
       application.update(status: "Approved!")
-      application_pets.each do |ap|
-        pet = Pet.find(ap.pet_id)
-        pet.update_adoptable
-        pet.save
-      end
+      confirm_adoptable(application_pets)
+    end
+  end
+
+  def confirm_adoptable(application_pets)
+    application_pets.each do |ap|
+      pet = Pet.find(ap.pet_id)
+      pet.update_adoptable
+      pet.save
     end
   end
 
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
+  before(:each) do
+    @application_1 = Application.create!(name: "Billy Bob",street_address:  "123 Main St",city: "Aurora",state: "CO",zip: "80012",description: "I like pets",status: "In Progress")
+    @application_2 = Application.create!(name: "John Mayer",street_address:  "123 Fake St",city: "Denver",state: "CO",zip: "80012",description: "I like pets",status: "Rejected")  
+    @application_3 = Application.create!(name: "Alex Smith",street_address:  "123 Grove St",city: "Boulder",state: "CO",zip: "80012",description: "I like pets",status: "Pending")
+    @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @pet_1 = @shelter_1.pets.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Bare-y Manilow')
+    @pet_2 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    @pet_3 = @shelter_1.pets.create(name: 'Heisenberg', breed: 'calico', age: 7, adoptable: true)
+    @app_pet_1 = ApplicationPet.create!(pet: @pet_1, application: @application_1)
+    @app_pet_2 = ApplicationPet.create!(pet: @pet_2, application: @application_1)
+  end
+
   describe "relationship" do
     it {should have_many(:application_pets)}
     it {should have_many(:pets).through(:application_pets)}
@@ -22,11 +34,65 @@ RSpec.describe Application, type: :model do
       zip: "80012",
       status: "In Progress",
       description: "I haven't chosen any pets",
-      status: "In Progress"
-  )
-
-  application_2.change_status_to_pending
-
-  expect(application_2.status).to eq("Pending")
+      )
+      application_2.change_status_to_pending
+      expect(application_2.status).to eq("Pending")
   end
+
+  describe "instance methods" do
+    describe "confirm_adoptable" do
+      xit "changes the status of all pets in application pets arguement to adoptable: false" do
+        @application_pets = [@app_pet_1, @app_pet_2]
+ 
+        expect(@app_pet_1.pet.adoptable).to eq(true)
+        expect(@app_pet_2.pet.adoptable).to eq(true)
+
+        @application_1.confirm_adoptable(@application_pets)
+
+        expect(@app_pet_1.pet.adoptable).to eq(false)
+        expect(app_pet_2.pet.adoptable).to eq(false)
+      end
+    end
+    describe "update_status" do
+      it "updates the status of application depending on if all application_pets have been approved " do
+        application_pets = [@app_pet_1, @app_pet_2]
+        @app_pet_1.approved = false
+        @app_pet_2.approved = true
+        @application_1.update_status(application_pets)
+        expect(@application_1.status).to eq("Rejected!")
+
+        application_pets = [@app_pet_1, @app_pet_2]
+        @app_pet_1.approved = true
+        @app_pet_2.approved = true
+        @application_1.update_status(application_pets)
+        expect(@application_1.status).to eq("Approved!")
+
+        application_pets = [@app_pet_1, @app_pet_2]
+        @app_pet_1.approved = nil
+        @app_pet_2.approved = true
+        @application_1.update_status(application_pets)
+        expect(@application_1.status).to eq("In Progress")
+      end
+
+      xit "calls confirm_adoptable which changes all pets status to adoptable:false if application is Approved!" do
+        application_pets = [@app_pet_1, @app_pet_2]
+        @app_pet_1.approved = true
+        @app_pet_2.approved = true
+        @application_1.update_status(application_pets)
+
+        expect(@application_1.status).to eq("Approved!")
+        expect(@app_pet_1.pet.adoptable).to eq(false)
+        expect(@app_pet_2.pet.adoptable).to eq(false)
+      end
+    end
+  end
+
+  describe "class methods" do
+    describe "::only_pending" do
+      it "returns applications that are pending only" do
+        expect(Application.only_pending.first).to eq(@application_3)
+      end
+    end
+  end
+
 end
