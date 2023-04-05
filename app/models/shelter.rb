c7b287e3d903cf75ec43eff60d91662e3c0dfe4c
Young Koh <youngheon6@gmail.com>
2023-04-05 11:18:11 -0600

    feat: Test and Implement User Story 22/23/24

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -21,6 +21,18 @@ class Shelter < ApplicationRecord
       .order("pets_count DESC")
   end
 
+  def self.join_application_pending
+    joins(pets: :applications).where("status = 'Pending'").order("name")
+  end
+  
+  def self.get_name(id)
+    Shelter.find_by_sql("SELECT name AS name FROM shelters WHERE id = #{id}").first.name
+  end
+  
+  def self.get_city(id)
+    Shelter.find_by_sql("SELECT city AS city FROM shelters WHERE id = #{id}").first.city
+  end
+
   def pet_count
     pets.count
   end
@@ -37,15 +49,15 @@ class Shelter < ApplicationRecord
     adoptable_pets.where('age >= ?', age_filter)
   end
 
-  def self.join_application_pending
-    joins(pets: :applications).where("status = 'Pending'").order("name")
+  def find_avg_age
+    pets.where("adoptable = #{true}").average(:age).to_f.round(2)
   end
 
-  def self.get_name(id)
-    Shelter.find_by_sql("SELECT name AS name FROM shelters WHERE id = #{id}").first.name
+  def adoptable_pet_count
+    pets.where("adoptable = #{true}").count
   end
 
-  def self.get_city(id)
-    Shelter.find_by_sql("SELECT city AS city FROM shelters WHERE id = #{id}").first.city
+  def adopted_pet_count
+    pets.where("adoptable = #{false}").count
   end
 end
